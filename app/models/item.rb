e824d38dcea21cf1ee5dfafa973d529afcf48deb
Nabil <50252768+nlabrazi@users.noreply.github.com>
2020-11-23 16:30:36 +0100

    Update item.rb
    missing plural

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
-  has_many   :order_item
-  has_many   :order, through: :order_item
-  has_many   :cart_item
-  has_many   :cart, through: :cart_item
+  has_many   :order_items
+  has_many   :order, through: :order_items
+  has_many   :cart_items
+  has_many   :cart, through: :cart_items
 end
