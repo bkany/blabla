95b511e0b62327d977267c190b6e2f2e64ceebc4
SimonMfroy <mainfroy.simon@gmail.com>
2020-11-23 16:27:44 +0100

    last correction again

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
   has_many   :order_item
-  has_many :order, through: :order_item
-  has_many :cart_item
-  has_many :cart, through: :cart_item
+  has_many   :order, through: :order_item
+  has_many   :cart_item
+  has_many   :cart, through: :cart_item
+end
