48941727f24713757220a0d9f40619f6cf266122
Nabil Labrazi <na.labrazi@gmail.com>
2020-11-24 12:17:03 +0100

    Full Process Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
-  has_many   :orders
-  has_one    :cart
+  has_many :orders
+  has_one  :cart
+
+  has_one_attached :avatar
+
+  #validates :first_name, presence: true
+  #validates :last_name, presence: true
+
 end
