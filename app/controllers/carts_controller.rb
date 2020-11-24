19d243161ff9c83ecefc9a5339db06da136e3b97
SimonMfroy <mainfroy.simon@gmail.com>
2020-11-24 12:52:53 +0100

    Creation Controller Carts and creation #ShowCarts -need: later verif to access for connected user only

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,36 @@
 class CartsController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+		@cart = @user.cart
+		@cart_items = @cart.items
+
+		#@cart_items.each |cart_item| do
+			# cart_item.title
+		#end 
+	end
+
+	def new
+		@cart_item = Cart_item.new
+	end
+
+	def create
+		@cart_item = Cart_item.new(cart_item_params)
+		#@event.admin = current_user
+
+		if @cart_item.save
+			flash.notice = "Votre produit a bien été ajouté à votre panier"
+			#redirect_to @event
+		else
+			flash.alert = "Une erreur est survenue #{@cart_item.errors.messages}"
+			render :show
+		end
+	end
+
+	private
+
+	def cart_item_params
+		params.require(:cart_id, :item_id)
+	end
+
 end
