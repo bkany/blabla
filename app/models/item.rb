56c719669adec56ab9751830b82b75cc93bf5446
Hyakon <azn.baoul@gmail.com>
2020-11-24 11:25:21 +0100

    item validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,16 @@ class Item < ApplicationRecord
   has_many   :orders, through: :order_items
   has_many   :cart_items
   has_many   :carts, through: :cart_items
+
+  # t.string "title"
+  # t.text "description"
+  # t.decimal "price"
+  # t.string "image_url"
+
+  validates :title, :description, :price, :image_url, presence: true
+  validates :title, length: { in: 3..30 }
+  validates :description, length: { in: 4..10000 }
+  validates :price, numericality: true
+  validates :image_url, uniqueness: true
+
 end
