a6bf64748c82b9357fcdef731ca08bfe1af640a9
bkany <bkany@student.42.fr>
2020-11-25 00:32:49 +0100

    Correction of the Migration on Heroku - changing the CreateTableUser file

--- db/migrate/20201123103613_create_users.rb
+++ db/migrate/20201123103613_create_users.rb
@@ -3,10 +3,11 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string   :first_name
       t.string   :last_name
-      t.string   :email
-      t.string   :password_digest
+      t.string 	 :email, null: false, default: ""
+      t.string 	 :encrypted_password, null: false, default: ""
       t.boolean  :is_admin
       t.string   :avatar
+      t.string   :remember_digest
 
       t.timestamps
     end
