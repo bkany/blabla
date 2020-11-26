a6647da012a7cb9f0ecdcaf6b89aef2ab22c73a8
rtdeedqgryet <borismariondorier@gmail.com>
2020-11-26 16:34:37 +0100

    finish dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,11 @@
 class DashboardsController < ApplicationController
+	before_action :authenticate_user!
+  include DashboardsHelper
+
   def index
+    if !current_user.is_admin
+      redirect_to root_path
+    end
+    @orders = Order.all
   end
 end
--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,12 @@
 module DashboardsHelper
+    def total
+        buff = 0
+        Order.all.each do |t|
+          buff += t.total_price
+        end 
+        return buff
+    end
+    def cart_average
+        return (total/Order.all.count).round(2)
+    end
 end
