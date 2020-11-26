f0984ae7903e11578e005bd2da32f91569d2415b
rtdeedqgryet <borismariondorier@gmail.com>
2020-11-26 16:01:05 +0100

    Add routes and generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboards/index'
   devise_for :users, controllers: {
       registrations: 'users/registrations'
   }
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
   	resources :cart_items, only: [:create, :update, :destroy]
   end
   resources :orders, only: [:show, :index, :create]
+  resources :dashboards, only: [:index]
 end
